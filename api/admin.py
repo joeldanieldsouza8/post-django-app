a5ae6ebb141f71bdd892bd6b377b3c577e62d6d3
Joel Dsouza <joeldsouza8@gmail.com>
2024-10-19 16:59:39 +0100

    added relations between tables

--- api/admin.py
+++ api/admin.py
@@ -1,19 +1,59 @@
+from typing import List, Optional
 from django.contrib import admin
-from .models import Post
+from django.db.models import QuerySet
+from .models import Post, Author, AuthorProfile, Tag
 
 
 # Register your models here.
 
+
 # Define the admin class for the Post model
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title',
-                    'importance_level']  # Define which fields to show in the list view. By including 'id', 'title', and 'importance_level', you get an overview of each post with these details displayed
-    list_filter = ['importance_level']  # Add a filter to filter posts by importance level
-    search_fields = ['title', ]  # Add a search bar to filter posts by title
+    list_display = [
+        "id",
+        "title",
+        "importance_level",
+        "author",
+        "get_tags",
+    ]  # Define which fields to show in the list view. By including 'id', 'title', and 'importance_level', you get an overview of each post with these details displayed
+    list_filter = [
+        "importance_level"
+    ]  # Add a filter to filter posts by importance level
+    search_fields = [
+        "title",
+    ]  # Add a search bar to filter posts by title
     list_per_page = 5  # Display 5 posts per page
     ordering = (
-        '-id',)  # Order the posts by id in descending order. Specifies the default ordering for the list view, with -id ordering posts by ID in descending order, so newer posts appear first
+        "-id",
+    )  # Order the posts by id in descending order. Specifies the default ordering for the list view, with -id ordering posts by ID in descending order, so newer posts appear first
+
+    @admin.display(description="Tags")
+    def get_tags(self, obj: Post) -> Optional[str]:
+        tags: QuerySet[Tag] = obj.tags.all() # Get all tags associated with the post
+        return ", ".join([tag.name for tag in tags]) if tags else None # Return a comma-separated string of tag names if tags exist, else return None
 
 
-# Register the Post model with the custom PostAdmin class in the admin site
+# Register the Post model with the custom PostAdmin class in  the admin site
 admin.site.register(Post, PostAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["id", "fullname", "email"]
+    search_fields = ["fullname", "email"]
+    list_per_page = 5
+    ordering = ("-id",)
+
+
+# Register the Author model with the custom AuthorAdmin class in the admin site
+admin.site.register(Author, AuthorAdmin)
+
+
+class AuthorProfileAdmin(admin.ModelAdmin):
+    list_display = ["author", "bio"]
+    search_fields = ["author", "bio"]
+    list_per_page = 5
+    ordering = ("-author",)
+
+
+# Register the AuthorProfile model with the custom AuthorProfileAdmin class in the admin site
+admin.site.register(AuthorProfile, AuthorProfileAdmin)
--- api/models.py
+++ api/models.py
@@ -4,16 +4,81 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+        ordering = ["fullname"]  # Order the authors by fullname in ascending order
+
+    def __str__(self):
+        return self.fullname
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+        ordering = ["name"]  # Order the tags by name in ascending order
+
+    def __str__(self):
+        return f"Tag: {self.name}"
+
+
+# def get_default_author():
+#     author, created = Author.objects.get_or_create(fullname='System Author', email="system@example.com")
+
+#     return author
+
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField(
+        Author, on_delete=models.CASCADE, related_name="author_profile"
+    )
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Author Profile"
+        verbose_name_plural = "Author Profiles"
+
+    def __str__(self):
+        return f"Profile of {self.author.fullname}"
+
+
 class Post(models.Model):
     class ImportanceLevel(models.IntegerChoices):
-        LOW = 1, _('Low')
-        MEDIUM = 2, _('Medium')
-        HIGH = 3, _('High')
+        LOW = 1, _("Low")
+        MEDIUM = 2, _("Medium")
+        HIGH = 3, _("High")
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     body = models.TextField()
-    importance_level = models.IntegerField(choices=ImportanceLevel.choices, default=ImportanceLevel.LOW)
+    importance_level = models.IntegerField(
+        choices=ImportanceLevel.choices, default=ImportanceLevel.LOW
+    )
+
+    # FK relationships
+    # first_arg: The model being referenced to which the relationship is being made
+    # on_delete: This ensures that if an Author is deleted, all related Posts are also deleted
+    # related_name: This allows you to access all the posts of an author using author.posts in your queries
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, blank=True, related_name="posts"
+    )
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = [
+            "-importance_level"
+        ]  # Order the posts by importance level in descending order
 
     # This dunder method is used to return a string representation of the instance/object of the Post class
     def __str__(self):
